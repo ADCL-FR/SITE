a7d043ba1c537450c93707714be9de7abc50e089
erisson <aime.risson.1@gmail.com>
2023-02-04 11:56:12 +0100

    faire des elements drag and drop réutilisable + style Zones

--- src/components/DND/Droppable.js
+++ src/components/DND/Droppable.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import { useDrag } from "react-dnd";
 
-
-export default function Droppable({children, item, style, type}){
-  const [{ isDragging }, dragRef] = useDrag({
-    type: "any",
+export default function Droppable({
+  children,
+  item,
+  style,
+  type,
+  isDragging = () => {},
+}) {
+  const [{ currentlyDragging }, dragRef] = useDrag({
+    type: type,
     item: item,
     collect: (monitor) => ({
-        isDragging: monitor.isDragging()
-    })
-  })
-  return (
-      <div style={style} ref={dragRef} key={item.id}>
+      currentlyDragging: monitor.isDragging(),
+    }),
+  });
 
-          {item.description}
-          {children}
-          {isDragging && '🥰🤓🧐'}
-          
-      </div>
-  )
-};
+  useEffect(() => {
+    isDragging(currentlyDragging);
+  }, [currentlyDragging]);
+  return (
+    <div style={style} ref={dragRef} key={item.id}>
+      {children}
+    </div>
+  );
+}
+
+// import React from 'react';
+// import { useDrag } from "react-dnd";
+
+// export default function Droppable({children, item, style, type}){
+//   const [{ isDragging }, dragRef] = useDrag({
+//     type: type,
+//     item: item,
+//     collect: (monitor) => ({
+//         isDragging: monitor.isDragging()
+//     })
+//   })
+//   return (
+//       <div style={style} ref={dragRef} key={item.id}>
+
+//           {item.description}
+//           {children}
+//           {isDragging && '🥰🤓🧐'}
+
+//       </div>
+//   )
+// };
---
+++ src/components/Zones/ZoneDropFiches.js
@@ -0,0 +1,85 @@
+// React
+import { faL } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect, useState } from "react";
+
+// drap and drop
+import { useDrop } from "react-dnd";
+import Droppable from "../DND/Droppable";
+import DropZone from "../DND/DropZone";
+import FicheDropRow from "./FicheDropRow";
+export default function ZoneDropFiches({
+  id,
+  accept,
+  children,
+  fiches,
+  title,
+  style,
+  onDrop,
+  onDeleteFiche,
+  isZone = false,
+}) {
+  const [isOver, setIsOver] = useState(false);
+  const [isDragging, setIsDragging] = useState(false);
+  const getBackgroundColor = () => {
+    if (isOver) {
+      return "#ECECEC";
+    } else {
+      return "";
+    }
+  };
+  return (
+    <DropZone
+      id={id}
+      accept={accept}
+      style={{
+        ...style,
+        backgroundColor: getBackgroundColor(),
+        overflow: "auto",
+      }}
+      isOver={(e) => setIsOver(e)}
+      onDrop={(e) => onDrop(e)}
+    >
+      <p style={title_style}>{title}</p>
+      {isZone && (
+        <div style={children_style}>
+          {fiches?.map((fiche) => {
+            return (
+              <FicheDropRow
+                fiche={fiche}
+                onDeleteFiche={(item) => onDeleteFiche(item)}
+                isPlanned={true}
+              />
+            );
+          })}
+          {children}
+        </div>
+      )}
+      {!isZone && (
+        <div style={children_style}>
+          {fiches?.map((fiche) => {
+            return (
+              <FicheDropRow
+                fiche={fiche}
+                onDeleteFiche={(item) => onDeleteFiche(item)}
+                isPlanned={false}
+              />
+            );
+          })}
+          {children}
+        </div>
+      )}
+    </DropZone>
+  );
+}
+
+const title_style = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  "border-bottom": "1px solid #000000",
+};
+
+const children_style = {
+  padding: "0px",
+};
