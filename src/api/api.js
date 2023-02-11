b1222082fc4546018e7de4172e29491c90771cf1
erisson <aime.risson.1@gmail.com>
2023-02-11 14:52:21 +0100

    ajout classs api

--- src/api/api.js
+++ src/api/api.js
@@ -1,27 +1,81 @@
 import axiosInstance from '../utils/axios';
 
-const nouvelle_affaire = async (affaire) => {
-    const response = await axiosInstance.post(`/api/affaires`, {...affaire})
-        .then((response) => {
-            return response.data;
-        })
-        .catch((error) => {
-            throw error;
-        });
-    return response;
+
+class affaire {
+    static nouvelle_affaire = async (affaire) => {
+        const response = await axiosInstance.post(`/api/affaires`, {...affaire})
+            .then((response) => {
+                return response.data;
+            })
+            .catch((error) => {
+                throw error;
+            });
+        return response;
+    }
+
+    static get_affaires = async (page=1, perPage=10, ordering="-date_rendu") => {
+        const response = await axiosInstance.get(`/api/affaires?page=${page}&per_page=${perPage}&ordering=${ordering}`)
+            .then((response) => {
+                return response.data;
+            })
+            .catch((error) => {
+                throw error;
+            });
+        return response;
+    };
+
+    static get_affaire = async (affaireId) => {
+        const response = await axiosInstance.get(`/api/affaires/${affaireId}`)
+            .then((response) => {
+                return response.data;
+            })
+            .catch((error) => {
+                throw error;
+            });
+        return response;
+    }
+
+
 }
 
-const get_affaires = async (page=1, perPage=10, ordering="-date_rendu") => {
-    const response = await axiosInstance.get(`/api/affaires?page=${page}&per_page=${perPage}&ordering=${ordering}`)
-        .then((response) => {
-            return response.data;
-        })
-        .catch((error) => {
-            throw error;
-        });
-    return response;
-};
+class fiche {
+   static get_fiche_etapes = async (ficheId) => {
+       const response = await axiosInstance.get(`/api/fiches/${ficheId}`)
+           .then((response) => {
+               return response.data;
+           })
+           .catch((error) => {
+               throw error;
+           });
+       return response;
+   }
+}
 
+class etape {
+    static nouvelle_etape = async (etape) => {
+        const response = await axiosInstance.post(`/api/etapes`, {...etape})
+            .then((response) => {
+                return response.data;
+            })
+            .catch((error) => {
+                throw error;
+            });
+        return response;
+    }
+}
+
+class machine {
+    static get_machines = async () => {
+        const response = await axiosInstance.get(`/api/machines`)
+            .then((response) => {
+                return response.data;
+            })
+            .catch((error) => {
+                throw error;
+            });
+        return response;
+    }
+}
 const recuperer_statuts = async () => {
     await axiosInstance.get(`/api/status`)
         .then((response) => {
@@ -110,7 +164,7 @@ const delete_affectation = async (affectationId) => {
     return response;
 }
 
-const fiches_a_planifier = async () => {
+const fiches_ajustage_a_planifier = async () => {
     const response = await axiosInstance.get(`/api/fiches/ajustage/a_planifier`)
         .then((response) => {
             return response.data;
@@ -122,16 +176,18 @@ const fiches_a_planifier = async () => {
 }
 
 const API = {
-    nouvelle_affaire,
+    affaire,
+    fiche,
+    etape,
+    machine,
     recuperer_statuts,
     recuperer_clients,
     recuperer_salaries,
-    get_affaires,
     planning_zone,
     get_zones,
     update_affectation,
     delete_affectation,
-    fiches_a_planifier,
+    fiches_ajustage_a_planifier,
     create_affectation
 };
 
