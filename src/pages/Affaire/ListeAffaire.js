588b57aa97b86d521da6c7449f6b3cf0acf09a63
Risson <aime.risson.1@gmail.com>
2023-03-10 13:53:07 +0100

    reformat

--- src/pages/Affaire/ListeAffaire.js
+++ src/pages/Affaire/ListeAffaire.js
@@ -1,192 +1,196 @@
 // React
-import React, {useEffect, useState} from "react";
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 // components
 import Page from "../Page";
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
 import Progress from "../../components/Elements/Progress";
 
 // sections
 // ----------------------------------------------------------------------
 import API from "../../api/api";
 import PageHeader from "../../components/Headers/PageHeader";
-import FichesTable from "../../components/Table/FichesTable"
+import FichesTable from "../../components/Table/FichesTable";
 
 // table
-import DataTable from 'react-data-table-component';
-import {PATH_DASHBOARD} from "../../routes/paths";
-const ExpandedComponent = ({ data }) => <FichesTable affaireId={data.id} />;
+import DataTable from "react-data-table-component";
+import { PATH_DASHBOARD } from "../../routes/paths";
+const ExpandedComponent = ({ data }) => (
+  <FichesTable affaireId={data.id} affaire={data} />
+);
 const columns = [
-    {
-        name: 'Numéro',
-        selector: row => row.num_affaire,
-        sortable: true,
-        sortField: 'num_affaire',
-        cell: row => (
-    		<a href={`/dashboard/affaire/${row.id}`} style={{color: "blue", textDecoration: "underline"}} >
-                {row.num_affaire}
-		    </a>
-	    )
-    },
-    {
-        name: 'Description',
-        selector: row => row.description || null,
-        hide: "md",
-        right: false,
-        grow: 2,
-    },
-    {
-        name: 'Avancement',
-        selector: row => row.avancement_affaire,
-        center: true,
-        sortable: false,
-        cell: row => (
-            <div className="flex items-center w-full">
-                <Progress value={row.avancement_affaire}  text={row.avancement_affaire == 0 ? " " : row.avancement_affaire} color="indigo" />
-            </div>
-        ),
-
-
-    },
-    {
-        name: 'Client',
-        selector: row => row.client_detail?.raison,
-        hide: "md",
-        center: true,
-    },
-    {
-        name: 'Chargé d\'affaire',
-        selector: row => row.charge_affaire_detail?.nom,
-        hide: 'sm',
-        center: true,
-    },
-    {
-        name: 'Date de création',
-        selector: row => row.date_creation,
-        hide: "md",
-        center: true,
-        //0sortable: true,
-    },
-    {
-        name: 'Délais',
-        selector: row => row.date_rendu,
-        hide: "md",
-        center: true,
-        sortable: true,
-        sortField: 'date_rendu',
-    },
-    {
-        name: 'Statut',
-        selector: row => row.statut_detail?.description,
-        center: true,
-        //sortable: true,
-    },
-
+  {
+    name: "Numéro",
+    selector: (row) => row.num_affaire,
+    sortable: true,
+    sortField: "num_affaire",
+    cell: (row) => (
+      <a
+        href={`/dashboard/affaire/${row.id}`}
+        style={{ color: "blue", textDecoration: "underline" }}
+      >
+        {row.num_affaire}
+      </a>
+    ),
+  },
+  {
+    name: "Description",
+    selector: (row) => row.description || null,
+    hide: "md",
+    right: false,
+    grow: 2,
+  },
+  {
+    name: "Avancement",
+    selector: (row) => row.avancement_affaire,
+    center: true,
+    sortable: false,
+    cell: (row) => (
+      <div className="flex items-center w-full">
+        <Progress
+          value={row.avancement_affaire}
+          text={row.avancement_affaire == 0 ? " " : row.avancement_affaire}
+          color="indigo"
+        />
+      </div>
+    ),
+  },
+  {
+    name: "Client",
+    selector: (row) => row.client_detail?.raison,
+    hide: "md",
+    center: true,
+  },
+  {
+    name: "Chargé d'affaire",
+    selector: (row) => row.charge_affaire_detail?.nom,
+    hide: "sm",
+    center: true,
+  },
+  {
+    name: "Date de création",
+    selector: (row) => row.date_creation,
+    hide: "md",
+    center: true,
+    //0sortable: true,
+  },
+  {
+    name: "Délais",
+    selector: (row) => row.date_rendu,
+    hide: "md",
+    center: true,
+    sortable: true,
+    sortField: "date_rendu",
+  },
+  {
+    name: "Statut",
+    selector: (row) => row.statut_detail?.description,
+    center: true,
+    //sortable: true,
+  },
 ];
 
-
-
 const Alert = React.forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={60} ref={ref} variant="filled" {...props} />;
+  return <MuiAlert elevation={60} ref={ref} variant="filled" {...props} />;
 });
 
 export default function ListeAffaire() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [affaires, setAffaires] = useState([]);
 
-    const [affaires, setAffaires] = useState([]);
+  // table
+  const [loading, setloading] = useState(false);
+  const [totalRows, setTotalRows] = useState(0);
+  const [perPage, setPerPage] = useState(10);
+  const [ordering, setOrdering] = useState("date_rendu");
+  const [direction, setDirection] = useState("-");
 
-    // table
-    const [loading, setloading] = useState(false);
-    const [totalRows, setTotalRows] = useState(0);
-    const [perPage, setPerPage] = useState(10);
-    const [ordering, setOrdering] = useState("date_rendu");
-    const [direction, setDirection]= useState('-')
-
-    //Alerte
-    const [open, setOpen] =useState(false);
-    const [message, setMessage] = useState('');
-    const [success, setSuccess] = useState(false);
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-        setOpen(false);
+  //Alerte
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
     }
+    setOpen(false);
+  };
 
-    const getAffaires = async (page, perPage, ordering, direction) => {
-        setloading(true)
-        const response = await API.affaire.get_affaires(page, perPage, direction+ordering).then((response) => {
-            setTotalRows(response.count)
-            setloading(false)
-           return response.results;
-        })
-        setAffaires(response);
-    };
+  const getAffaires = async (page, perPage, ordering, direction) => {
+    setloading(true);
+    const response = await API.affaire
+      .get_affaires(page, perPage, direction + ordering)
+      .then((response) => {
+        setTotalRows(response.count);
+        setloading(false);
+        return response.results;
+      });
+    setAffaires(response);
+  };
 
-    const handleSort = (column, sortDirection) => {
-        // simulate server sort
+  const handleSort = (column, sortDirection) => {
+    // simulate server sort
 
-        setOrdering(column.sortField)
-        setDirection(sortDirection === 'asc' ? '-' : '')
-        // instead of setTimeout this is where you would handle your API call.
+    setOrdering(column.sortField);
+    setDirection(sortDirection === "asc" ? "-" : "");
+    // instead of setTimeout this is where you would handle your API call.
+  };
 
-    };
+  const handleAffaireClick = (row) => {
+    navigate(PATH_DASHBOARD.affaire.view(row.id));
+  };
 
-    const handleAffaireClick = row => {
-        navigate(PATH_DASHBOARD.affaire.view(row.id))
-    }
+  useEffect(() => {
+    getAffaires(1, perPage, ordering, direction);
+  }, [perPage, ordering, direction]);
 
-
-    useEffect(() => {
-        getAffaires(1, perPage, ordering, direction);
-    }, [perPage, ordering, direction]);
-
-    return (
-        <Page title="Liste des Affaires" className="flex flex-col h-full bg-blueGray-100">
-            <PageHeader title="Affaires" />
-            <div className="md:px-10 mb-20" style={{"marginTop": "-8rem"}}>
-                <Snackbar
-                    open={open}        
-                    autoHideDuration={4000}
-                    message=""
-                    onClose={handleClose}
-                >
-                    <Alert onClose={handleClose} severity={success ? 'success' : 'error' } sx={{ width: '100%' }}>
-                        {message}
-                    </Alert>
-                </Snackbar>
-                <DataTable
-                    style={{borderRadius: "15px"}}
-                    title={"Liste des affaires"}
-                    columns={columns}
-                    data={affaires}
-                    expandableRows
-                    expandableRowsComponent={ExpandedComponent}
-                    pagination={true}
-                    paginationServer
-                    paginationTotalRows={totalRows}
-                    onChangePage={(page) => getAffaires(page, perPage)}
-                    onChangeRowsPerPage={(nb_page) => setPerPage(nb_page)}
-                    progressPending={loading}
-                    sortServer
-                    onSort={handleSort}
-                    fixedHeader
-                    fixedHeaderScrollHeight="500px"
-                    expandOnRowClicked
-                    striped
-                    onRowDoubleClicked={(row) => handleAffaireClick(row)}
-
-
-                />
-                {/*<Table head={tableAffaireHeaders} title={tableAffaireTitle} body={affaires} RowComponent={AffaireRow}/>*/}
-            </div>
-
-
-
-        </Page>
-    );
-
+  return (
+    <Page
+      title="Liste des Affaires"
+      className="flex flex-col h-full bg-blueGray-100"
+    >
+      <PageHeader title="Affaires" />
+      <div className="md:px-10 mb-20" style={{ marginTop: "-8rem" }}>
+        <Snackbar
+          open={open}
+          autoHideDuration={4000}
+          message=""
+          onClose={handleClose}
+        >
+          <Alert
+            onClose={handleClose}
+            severity={success ? "success" : "error"}
+            sx={{ width: "100%" }}
+          >
+            {message}
+          </Alert>
+        </Snackbar>
+        <DataTable
+          style={{ borderRadius: "15px" }}
+          title={"Liste des affaires"}
+          columns={columns}
+          data={affaires}
+          expandableRows
+          expandableRowsComponent={ExpandedComponent}
+          pagination={true}
+          paginationServer
+          paginationTotalRows={totalRows}
+          onChangePage={(page) => getAffaires(page, perPage)}
+          onChangeRowsPerPage={(nb_page) => setPerPage(nb_page)}
+          progressPending={loading}
+          sortServer
+          onSort={handleSort}
+          fixedHeader
+          fixedHeaderScrollHeight="500px"
+          expandOnRowClicked
+          striped
+          onRowDoubleClicked={(row) => handleAffaireClick(row)}
+        />
+        {/*<Table head={tableAffaireHeaders} title={tableAffaireTitle} body={affaires} RowComponent={AffaireRow}/>*/}
+      </div>
+    </Page>
+  );
 }
--- src/pages/Fiche/FicheDetail.js
+++ src/pages/Fiche/FicheDetail.js
@@ -1,19 +1,17 @@
 // React
-import React, {useEffect, useState} from "react";
-import {
-    useParams
-} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 // components
 import Page from "../Page";
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
 import Button from "../../components/Elements/Button";
-import Dialog from '@mui/material/Dialog';
+import Dialog from "@mui/material/Dialog";
 import FormCard from "../../components/Form/FormCard";
 import FicheForm from "../../components/Form/fiche/FicheForm";
 import Input from "../../components/Elements/Input";
 //constants
-import {etapeForm} from '../../constants/forms/forms'
+import { etapeForm } from "../../constants/forms/forms";
 // sections
 // ----------------------------------------------------------------------
 
@@ -21,196 +19,219 @@ import API from "../../api/api";
 import PageHeader from "../../components/Headers/PageHeader";
 
 // table
-import DataTable from 'react-data-table-component';
-import FichesTable from "../../components/Table/FichesTable";
+import DataTable from "react-data-table-component";
 
-const columns = [{
-    name: 'Numéro', selector: row => row.num_etape, sortable: true,
-}, {
-    name: 'Machine', selector: row => row.machine?.nom_machine || null, center: true
-
-
-}, {
-    name: 'Quantité', selector: row => row.quantite, center: true,
-}, {
-    name: 'Temps', selector: row => row.temps, center: true,
-}, {
-    name: 'Terminée', selector: row => row.terminee ? 'Oui' : 'Non', center: true,
-}
-
+const columns = [
+  {
+    name: "Numéro",
+    selector: (row) => row.num_etape,
+    sortable: true,
+  },
+  {
+    name: "Machine",
+    selector: (row) => row.machine?.nom_machine || null,
+    center: true,
+  },
+  {
+    name: "Quantité",
+    selector: (row) => row.quantite,
+    center: true,
+  },
+  {
+    name: "Temps",
+    selector: (row) => row.temps,
+    center: true,
+  },
+  {
+    name: "Terminée",
+    selector: (row) => (row.terminee ? "Oui" : "Non"),
+    center: true,
+  },
 ];
 
-
 const Alert = React.forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={60} ref={ref} variant="filled" {...props} />;
+  return <MuiAlert elevation={60} ref={ref} variant="filled" {...props} />;
 });
 
 const ExpandedEtape = ({ data }) => (
-    <div style={syle_expanded_container}>
-    <Input disabled type={"textarea"} value={data.description}/>
-    </div>);
+  <div style={syle_expanded_container}>
+    <Input disabled type={"textarea"} value={data.description} />
+  </div>
+);
 
 const syle_expanded_container = {
-    width: '100%',
-}
+  width: "100%",
+};
 export default function FicheDetail() {
+  let { id } = useParams();
+  const [etapes, setEtapes] = useState([]);
+  const [infos, setInfos] = useState({});
 
-    let {id} = useParams()
-    const [etapes, setEtapes] = useState([]);
-    const [infos, setInfos] = useState({});
+  //form etape
+  const [showModal, setShowModal] = useState(false);
+  const [showModalFiche, setShowModalFiche] = useState(false);
+  const [formData, setFormData] = useState({});
+  const [formulaire, setFormulaire] = useState(etapeForm);
 
-    //form etape
-    const [showModal, setShowModal] = useState(false)
-    const [showModalFiche, setShowModalFiche] = useState(false)
-    const [formData, setFormData] = useState({});
-    const [formulaire, setFormulaire] = useState(etapeForm);
+  // table
+  const [loading, setloading] = useState(false);
 
-    // table
-    const [loading, setloading] = useState(false);
+  //Alerte
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState(false);
 
-    //Alerte
-    const [open, setOpen] = useState(false);
-    const [message, setMessage] = useState('');
-    const [success, setSuccess] = useState(false);
-
-    // form
-    const handleChange = (event) => {
-        setFormData({...formData, [event.target.id]: event.target.value});
-    };
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const form = {...formData, fiche: infos.id}
-        API.etape.nouvelle_etape(form).then((response) => {
-            setMessage('Étape ajoutée avec succès');
-            setSuccess(true);
-            setOpen(true);
-            setShowModal(false)
-            getEtapes(id)
-        }).catch((error) => {
-            setMessage('Erreur lors de la création de l\'étape');
-            setSuccess(false);
-            setOpen(true);
-        });
-    }
-    const handleSelectChange = (value, id) => {
-        setFormData({...formData, [id]: value});
-    };
-    const setMachineOptions = async () => {
-        const machines = await API.machine.get_machines()
-        let options = [];
-        machines.results.map((machine) => {
-            options.push({value: machine.id, label: machine.nom_machine})
-        });
-        const newForm = {...formulaire};
-        newForm.forms[0].inputs.map((input) => {
-            if (input.select && input.select.id === "machine") {
-                input.select.options = options;
-            }
-        });
-        setFormulaire(newForm);
-    }
-
-    // alert fiche
-    const handleFicheSubmitResponse = async (succes, message) => {
-       if(succes){
-           setMessage('Action réalisée avec succès');
-           setSuccess(true);
-           setOpen(true);
-           setShowModalFiche(false)
-           getEtapes(id)
-         }else{
-           setMessage('Erreur lors de l\'action');
-           setSuccess(false);
-           setOpen(true);
-       }
-    }
-
-    //alert
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-        setOpen(false);
-    }
-
-    function actions() {
-        return <div>
-            <Button color="orange" onClick={() => setShowModalFiche(true)}>éditer fiche</Button>
-            <Button
-                onClick={() => setShowModal(true)}>+ étape
-            </Button>
-        </div>
-    }
-
-    const getEtapes = async (ficheId) => {
-        setloading(true)
-        API.fiche.get_fiche_etapes(ficheId).then((response) => {
-            setEtapes(response.etapes);
-            response.etapes = []
-            setInfos(response)
-            setloading(false)
-        })
-
-    };
-
-
-
-
-    useEffect(() => {
+  // form
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.id]: event.target.value });
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const form = { ...formData, fiche: infos.id };
+    API.etape
+      .nouvelle_etape(form)
+      .then((response) => {
+        setMessage("Étape ajoutée avec succès");
+        setSuccess(true);
+        setOpen(true);
+        setShowModal(false);
         getEtapes(id);
-        setMachineOptions();
-    }, [id]);
+      })
+      .catch((error) => {
+        setMessage("Erreur lors de la création de l'étape");
+        setSuccess(false);
+        setOpen(true);
+      });
+  };
+  const handleSelectChange = (value, id) => {
+    setFormData({ ...formData, [id]: value });
+  };
+  const setMachineOptions = async () => {
+    const machines = await API.machine.get_machines();
+    let options = [];
+    machines.results.map((machine) => {
+      options.push({ value: machine.id, label: machine.nom_machine });
+    });
+    const newForm = { ...formulaire };
+    newForm.forms[0].inputs.map((input) => {
+      if (input.select && input.select.id === "machine") {
+        input.select.options = options;
+      }
+    });
+    setFormulaire(newForm);
+  };
 
-    return (<Page title="Détail Fiche" className="flex flex-col h-full bg-blueGray-100">
-            <PageHeader title={`Affaire ${infos.num_affaire}`}/>
-            <div className="md:px-10 mb-20" style={{"marginTop": "-8rem"}}>
-                <Snackbar
-                    open={open}
-                    autoHideDuration={4000}
-                    message=""
-                    onClose={handleClose}
-                >
-                    <Alert onClose={handleClose} severity={success ? 'success' : 'error'} sx={{width: '100%'}}>
-                        {message}
-                    </Alert>
-                </Snackbar>
-                <DataTable
-                    title={`Étapes de ${infos.titre}`}
-                    columns={columns}
-                    data={etapes}
-                    progressPending={loading}
-                    expandableRows
-                    expandableRowsComponent={ExpandedEtape}
-                    expandOnRowClicked
-                    selectableRows={true}
-                    dense
-                    highlightOnHover
-                    pointerOnHover
-                    actions={actions()}
-                />
+  // alert fiche
+  const handleFicheSubmitResponse = async (succes, message) => {
+    if (succes) {
+      setMessage("Action réalisée avec succès");
+      setSuccess(true);
+      setOpen(true);
+      setShowModalFiche(false);
+      getEtapes(id);
+    } else {
+      setMessage("Erreur lors de l'action");
+      setSuccess(false);
+      setOpen(true);
+    }
+  };
 
-                <Dialog open={showModal} onClose={() => setShowModal(false)}>
-                    <FormCard {...etapeForm} onSubmit={handleSubmit} onChange={handleChange}
-                              onSelect={handleSelectChange}/>
-                </Dialog>
-                <Dialog open={showModalFiche} onClose={() => setShowModalFiche(false)}>
-                    <FicheForm ficheData={infos} update={true} onUpdated={handleFicheSubmitResponse} onCreated={handleFicheSubmitResponse}/>
-                </Dialog>
-            </div>
+  //alert
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
 
+  function actions() {
+    return (
+      <div>
+        <Button color="orange" onClick={() => setShowModalFiche(true)}>
+          éditer fiche
+        </Button>
+        <Button onClick={() => setShowModal(true)}>+ étape</Button>
+      </div>
+    );
+  }
 
-        </Page>);
+  const getEtapes = async (ficheId) => {
+    setloading(true);
+    API.fiche.get_fiche_etapes(ficheId).then((response) => {
+      setEtapes(response.etapes);
+      response.etapes = [];
+      setInfos(response);
+      setloading(false);
+    });
+  };
 
+  useEffect(() => {
+    getEtapes(id);
+    setMachineOptions();
+  }, [id]);
+
+  return (
+    <Page title="Détail Fiche" className="flex flex-col h-full bg-blueGray-100">
+      <PageHeader title={`Affaire ${infos.num_affaire}`} />
+      <div className="md:px-10 mb-20" style={{ marginTop: "-8rem" }}>
+        <Snackbar
+          open={open}
+          autoHideDuration={4000}
+          message=""
+          onClose={handleClose}
+        >
+          <Alert
+            onClose={handleClose}
+            severity={success ? "success" : "error"}
+            sx={{ width: "100%" }}
+          >
+            {message}
+          </Alert>
+        </Snackbar>
+        <DataTable
+          title={`Étapes de ${infos.titre}`}
+          columns={columns}
+          data={etapes}
+          progressPending={loading}
+          expandableRows
+          expandableRowsComponent={ExpandedEtape}
+          expandOnRowClicked
+          selectableRows={true}
+          dense
+          highlightOnHover
+          pointerOnHover
+          actions={actions()}
+        />
+
+        <Dialog open={showModal} onClose={() => setShowModal(false)}>
+          <FormCard
+            {...etapeForm}
+            onSubmit={handleSubmit}
+            onChange={handleChange}
+            onSelect={handleSelectChange}
+          />
+        </Dialog>
+        <Dialog open={showModalFiche} onClose={() => setShowModalFiche(false)}>
+          <FicheForm
+            ficheData={infos}
+            update={true}
+            onUpdated={handleFicheSubmitResponse}
+            onCreated={handleFicheSubmitResponse}
+          />
+        </Dialog>
+      </div>
+    </Page>
+  );
 }
 
 const modal_style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
 };
